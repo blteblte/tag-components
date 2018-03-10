c4efcac7db200bb4059cee9dcc3262a151f62eeb
Martin <martin.sky@inbox.lv>
2018-03-11 00:29:43 +0200

    to obj.composition

--- src/components/wrsts-tab-control/wrsts-tab-control.ts
+++ src/components/wrsts-tab-control/wrsts-tab-control.ts
@@ -1,42 +1,108 @@
 import { hasClass, addClass, removeClass, emitEvent } from '../../lib/dom';
+import '../../lib/polyfills/array#findIndex';
+import '../../lib/polyfills/array#find';
+import './wrsts-tab-control.scss'
 
-function bind() {
+interface TabControlState {
+  target: HTMLElement
+  tabs: HTMLElement[]
+  contents: HTMLElement[]
+  activeIndex: number
+}
+
+const tabControl = (target: HTMLElement) => {
+  let state: TabControlState = {
+      target
+    , tabs:     Array.prototype.slice.call(
+                  target.children[0].children
+                ) as HTMLElement[]
+    , contents: Array.prototype.slice.call(
+                  target.children[1].children
+                ) as HTMLElement[]
+    , activeIndex: -1
+  }
+  state.activeIndex = state.tabs.findIndex(t => hasClass(t, 'active'))
+  return { ...state
+    , ...canToggleClass(state, 'active', ['tabs', 'contents'])
+    , ...canRoute(state, 'click', ['tabs'])
+  }
+}
+
+const canToggleClass = (state, className: string, arrKeys: string[]) => ({
+  toggleClass: (index) => {
+    arrKeys.forEach(key => {
+      state[key].forEach((x, i) => {
+        if (i === index) !hasClass(x, className) && addClass(x, className)
+        else hasClass(x, className) && removeClass(x, className)
+      })
+    })
+  }
+})
+
+interface RouteCallback {
+  routeHash: string
+  key: string
+  target: HTMLElement
+  index: number
+}
+
+const canRoute = (state, eventName: string, arrKeys: string[]) => ({
+
+  route: (callback: (route: RouteCallback) => any) => {
+    arrKeys.forEach(key => {
+      const routes: RouteCallback[] = []
+      state[key].forEach((x, i) => {
+        const routeHash = x.getAttribute('route')
+        if (routeHash !== null) {
+          routes.push({ key, routeHash, target: x, index: i })
+          x.addEventListener(eventName, () => { window.location.href = `#/${routeHash}` })
+        }
+      })
+      if (routes.length) {
+        /*  */
+        const initialRouteHash = (window.location.hash || '').replace('#/', '').toLowerCase()
+        if (initialRouteHash !== '') {
+          const route = routes.find(x => x.routeHash.toLowerCase() ===  initialRouteHash)
+          if(route) { callback(route) }
+        }
+        /*  */
+        window.addEventListener('hashchange', () => {
+          const routeHash = (window.location.hash || '').replace('#/', '')
+          const route = routes.find(x => x.routeHash.toLowerCase() === routeHash.toLowerCase())
+          if (route) { callback(route) }
+        })
+
+      }
+    })
+  }
+
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+
   Array.prototype.slice.call(
     document.querySelectorAll('wrsts-tab-control')
-  ).forEach(target => bindTabControl(target))
-}
+  ).forEach(target => {
 
-function bindTabControl(target: HTMLElement) {
+    const control = tabControl(target)
 
-  const tabs = Array.prototype.slice.call(
-    target.children[0].children
-  ) as HTMLElement[]
-
-  const contents = Array.prototype.slice.call(
-    target.children[1].children
-  ) as HTMLElement[]
-
-  let activeIndex = tabs.findIndex(t => hasClass(t, 'active'))
-
-  tabs.forEach((tab, clickIndex) => {
-    tab.addEventListener('click', () => {
-      tabs.forEach((x, i) => {
-        if (i === clickIndex) !hasClass(x, 'active') && addClass(x, 'active')
-        else hasClass(x, 'active') && removeClass(x, 'active')
-      })
-      contents.forEach((x, i) => {
-        if (i === clickIndex) !hasClass(x, 'active') && addClass(x, 'active')
-        else hasClass(x, 'active') && removeClass(x, 'active')
-      })
-
-      if (clickIndex !== activeIndex) {
-        emitEvent(target, 'wrsts:change')
+    const handleEventDispatching = (index) => {
+      if (control.activeIndex !== index) {
+        emitEvent(control.target, 'wrsts:change')
       }
-      activeIndex = clickIndex
+      control.activeIndex = index
+    }
+
+    control.tabs.forEach((t, i) => t.addEventListener('click', () => {
+      control.toggleClass(i)
+      handleEventDispatching(i)
+    }))
+
+    control.route((route) => {
+      control.toggleClass(route.index)
+      handleEventDispatching(route.index)
     })
+
   })
-}
 
-
-import './wrsts-tab-control.scss'
-document.addEventListener('DOMContentLoaded', bind)
+})
